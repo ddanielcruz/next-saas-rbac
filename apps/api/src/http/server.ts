d19911f61b7d790777fdb4139100681241066938
Daniel Cruz <danielcunha54@gmail.com>
2024-07-11 09:41:52 -0300

    docs: add swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,6 +1,13 @@
 import fastifyCors from '@fastify/cors';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 import { fastify } from 'fastify';
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod';
 
 import { createAccount } from './routes/auth/create-account';
 
@@ -8,6 +15,28 @@ export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    openapi: '3.1.0',
+    info: {
+      title: 'Next.js RBAC Saas',
+      description: 'Fullstack RBAC Saas with Next.js, Prisma, and Fastify',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+});
 
 app.register(fastifyCors, { origin: '*' });
 
