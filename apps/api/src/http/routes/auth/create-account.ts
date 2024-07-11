ec8cc6e9433822bf50ba776541dd2fd0cb193ef2
Daniel Cruz <danielcunha54@gmail.com>
2024-07-11 09:18:47 -0300

    feat: create account route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs';
 import type { FastifyInstance } from 'fastify';
 import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { z } from 'zod';
+
+import { prisma } from '@/lib/prisma';
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,22 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    async () => {
-      return { message: 'User created' };
+    async (request, reply) => {
+      const { email, name, password } = request.body;
+      const userWithSameEmail = await prisma.user.findUnique({
+        select: { id: true },
+        where: { email },
+      });
+
+      if (userWithSameEmail) {
+        return reply.status(409).send({ message: 'Email already in use.' });
+      }
+
+      const passwordHash = await hash(password, 10);
+
+      await prisma.user.create({ data: { email, name, passwordHash } });
+
+      return reply.status(201).send();
     },
   );
 }
